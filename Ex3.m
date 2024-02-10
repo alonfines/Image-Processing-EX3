72ec5f32502aadc19d5a3c98292ddb5c10a969cc
bennypardilov <benny.pardilov@gmail.com>
2024-02-10 19:59:45 +0200

    Ex3 MATLAB

---
+++ Ex3.m
@@ -0,0 +1,9 @@
+clc;
+clear all;
+close all;
+
+% Displaying the names of the submitters:
+disp('Alon Finestein - 318354057')
+disp('Binyamin Pardilov - 316163914')
+
+%% ------------------------------------------------------
